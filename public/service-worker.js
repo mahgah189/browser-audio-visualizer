f0b5cbb0b91d6fdfa478e56294933976a1cd98e3
Mark Baek <markbaek@Mac.verizon.net>
2025-06-27 21:59:45 -0400

    removed extension popup to initiate service worker on icon click

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+console.log("[service-worker] running");
+console.log("chrome.offscreen:", chrome.offscreen);
+
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log('[service-worker] icon clicked');
+
+  // check contexts for an offscreen document
+  const getContextsAsync = () => {
+    return new Promise((resolve) => {
+      chrome.runtime.getContexts({}, resolve);
+    });
+  }
+
+  const existingContexts = await getContextsAsync();
+  let recording = false;
+
+  // return true if an offscreen document was found
+  const offscreenDocument = existingContexts.find((context) => {
+    return context.contextType === "OFFSCREEN_DOCUMENT";
+  });
+
+  if (!offscreenDocument) {
+    await chrome.offscreen.createDocument({
+      url: "offscreen.html",
+      reasons: ["USER_MEDIA"],
+      justification: "Capturing audio from chrome.tabCapture API"
+    });
+  } else {
+    recording = offscreenDocument.documentUrl.endsWith("#recording");
+  }
+
+  if (recording) {
+    chrome.runtime.sendMessage({
+      type: "stop-recording",
+      target: "offscreen"
+    });
+    return;
+  }
+
+  const streamId = await chrome.tabCapture.getMediaStreamId({
+    targetTabId: tab.id
+  });
+
+  console.log(streamId);
+
+  chrome.runtime.sendMessage({
+    type: "start-recording",
+    target: "offscreen",
+    data: streamId
+  });
+});
+
+console.log('[service-worker] loaded');
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('[service-worker] onInstalled fired');
+});
--- src/components/buttons/BtnCaptureAudio/BtnCaptureAudio.jsx
+++ src/components/buttons/BtnCaptureAudio/BtnCaptureAudio.jsx
@@ -2,56 +2,70 @@ import React from "react";
 import "./BtnCaptureAudio.css";
 
 function BtnCaptureAudio(props) {
-  const [record, toggleRecord] = React.useState(false);
+  // const [record, toggleRecord] = React.useState(false);
 
-  React.useEffect(() => {
-    toggleTabAudioCapture();
-  }, [record]);
+  // React.useEffect(() => {
+  //   toggleTabAudioCapture();
+  // }, [record]);
 
-  const toggleTabAudioCapture = async (tab) => {
-    // check contexts for an offscreen document
-    const existingContexts = await chrome.runtime.getContexts();
-    let recording = false;
+  // const toggleTabAudioCapture = async () => {
+  //   const arrayTab = await chrome.tabs.query({
+  //     active: true,
+  //     currentWindow: true
+  //   });
 
-     // return true if an offscreen document was found
-    const offscreenDocument = existingContexts.find((context) => {
-      return context.contextType === "OFFSCREEN_DOCUMENT";
-    });
+  //   const tab = arrayTab[0];
+  //   console.log(tab);
 
-    if (!offscreenDocument) {
-      await chrome.offscreen.createDocument({
-        url: "offscreen.html",
-        reasons: ["USER_MEDIA"],
-        justification: "Capturing audio from chrome.tabCapture API"
-      });
-    } else {
-      recording = offscreenDocument.documentUrl.endsWith("#recording");
-    }
+  //   // check contexts for an offscreen document
+  //   const getContextsAsync = () => {
+  //     return new Promise((resolve) => {
+  //       chrome.runtime.getContexts({}, resolve);
+  //     });
+  //   }
 
-    if (recording) {
-      chrome.runtime.sendMessage({
-        type: "stop-recording",
-        target: "offscreen"
-      });
-      return;
-    }
+  //   const existingContexts = await getContextsAsync();
+  //   let recording = false;
 
-    const streamId = await chrome.tabCapture.getMediaStreamId({
-      targetTabId: tab.id
-    });
+  //    // return true if an offscreen document was found
+  //   const offscreenDocument = existingContexts.find((context) => {
+  //     return context.contextType === "OFFSCREEN_DOCUMENT";
+  //   });
 
-    chrome.runtime.sendMessage({
-      type: "start-recording",
-      target: "offscreen",
-      data: streamId
-    });
-  };
+  //   if (!offscreenDocument) {
+  //     await chrome.offscreen.createDocument({
+  //       url: "offscreen.html",
+  //       reasons: ["USER_MEDIA"],
+  //       justification: "Capturing audio from chrome.tabCapture API"
+  //     });
+  //   } else {
+  //     recording = offscreenDocument.documentUrl.endsWith("#recording");
+  //   }
+
+  //   if (recording) {
+  //     chrome.runtime.sendMessage({
+  //       type: "stop-recording",
+  //       target: "offscreen"
+  //     });
+  //     return;
+  //   }
+
+  //   const streamId = await chrome.tabCapture.getMediaStreamId({
+  //     targetTabId: tab.id
+  //   });
+
+  //   chrome.runtime.sendMessage({
+  //     type: "start-recording",
+  //     target: "offscreen",
+  //     data: streamId
+  //   });
+  // };
   
   return (
     <button
-      onClick={() => {
-        toggleRecord(!record);
-      }}
+      // onClick={() => {
+      //   toggleRecord(!record);
+      // }}
     >
       {props.children}
     </button>
